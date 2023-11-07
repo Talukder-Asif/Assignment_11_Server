a8ed9e273fd15cccfe52d3bdab023a9cae723a40
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2023-11-08 03:27:52 +0600

    delete food done

--- index.js
+++ index.js
@@ -154,8 +154,14 @@ async function run() {
     });
 
 
+// delete item of cliend from server
+app.delete('/food/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await dataCollection.deleteOne(query);
+  res.send(result);
+})
 
-    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
